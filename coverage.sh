278dae359c1777afd9664fb96cbb37f8742ab067
Eric Seidel <eric@shorebird.dev>
2023-09-11 20:42:15 -0700

    chore: use flutter instead of dart to run tests

--- coverage.sh
+++ coverage.sh
@@ -4,9 +4,5 @@
 # This can be used for viewing coverage locally in your editor.
 
 dart pub global activate coverage
-dart pub get
-dart test --coverage=coverage
-dart pub global run coverage:format_coverage --lcov --in=coverage \
-    --out=coverage/lcov.info --packages=.dart_tool/package_config.json \
-    --report-on=lib \
-    --check-ignore
+flutter packages get
+flutter test --coverage
